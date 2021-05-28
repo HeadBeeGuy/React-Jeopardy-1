3824a0d8f2652434dc635bebb56e9eb8684f2fc6
HeadBeeGuy <35615790+HeadBeeGuy@users.noreply.github.com>
2021-05-28 09:21:53 +0900

    Turn Category into a normal function

--- src/Category.js
+++ src/Category.js
@@ -1,31 +1,20 @@
-import React, { Component } from 'react'
 
-function Question(props) {
-  return (
-    <p>${props.value} - {props.question}</p>
+function Category(props) {
+  const data = props.data
+
+  const questions = data.questions.map((question) => 
+    <li key={question.value} className="question-block">
+      <span className="question-value">${question.value}</span>
+    </li>
   )
-}
-
-export class Category extends Component {
-
-
-  render() {
-    const data = this.props.data
-
-    const questions = data.questions.map((question) => 
-      <li key={question.value} className="question-block">
-        <span className="question-value">${question.value}</span>
-      </li>
-    )
-    return (
-      <div className="category-section">
-        <h3>{data.title}</h3>
-        <ol>
-          {questions}
-        </ol>
-      </div>
-    )
-  }
+  return (
+    <div className="category-section">
+      <h3>{data.title}</h3>
+      <ol>
+        {questions}
+      </ol>
+    </div>
+  )
 }
 
 export default Category
